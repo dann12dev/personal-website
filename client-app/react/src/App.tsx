1c7fce59c7ba387dc8acad2c998a53815066a681
daniel <daniel.huang@hy-tech.com.tw>
2026-04-16 12:02:09 +0800

    [project] 建立React + shadcn專案

---
+++ client-app/react/src/App.tsx
@@ -0,0 +1,32 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Dann</h1>
+      <Card size="sm" className="mx-auto w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+          <CardAction>Card Action</CardAction>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
+
+export default App
