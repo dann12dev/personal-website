a524aaee74bef73d2fe79ae1e384050a83ac4ad5
daniel <daniel.huang@hy-tech.com.tw>
2026-05-04 11:43:50 +0800

    [feature] 修改Menu大版切換時會有一瞬間水平overflow問題

--- client-app/react/src/components/layout/AppLayout.tsx
+++ client-app/react/src/components/layout/AppLayout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/layout/Footer";
 const AppLayout = () => {
   return (
     <>
-      <div className="flex flex-col min-h-screen w-full lg:max-w-[1024px] xl:max-w-[1280px] mx-auto min-w-xs">
+      <div className="flex flex-col min-h-screen w-full lg:max-w-[1024px] xl:max-w-[1280px] mx-auto min-w-xs overflow-x-clip">
         <Header></Header>
         <main className="flex-1 w-full px-4 md:px-8 lg:px-12">
           <Outlet />
