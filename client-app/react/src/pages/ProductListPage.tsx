2e4f01e47c6f9c6b2cc8b03c6c390d78dd41395b
dann12dev <dann12dev@gmail.com>
2026-05-06 22:57:43 +0800

    [feature] 加入多筆Product Card展示

--- client-app/react/src/pages/ProductListPage.tsx
+++ client-app/react/src/pages/ProductListPage.tsx
@@ -9,13 +9,33 @@ import {
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 
+type CardItem = {
+  src?: string,
+  tag?: string,
+  title?: string,
+  desc?: string 
+}
+
+const cards: Array<CardItem> = [
+{ src: "https://picsum.photos/1000/600", title: "product-1", tag: "good" },
+{ src: "https://picsum.photos/768/400", title: "product-2", tag: "good" },
+{ src: "https://picsum.photos/768/800", title: "product-3" },
+{ src: "https://picsum.photos/768/500", title: "product-4" },
+{ src: "https://picsum.photos/768/200", title: "product-5" },
+{ src: "https://picsum.photos/768/400", title: "product-6" },
+{ src: "https://picsum.photos/768/400", title: "product-7" },
+{ src: "https://picsum.photos/500/400", title: "product-8" },
+{ src: "https://picsum.photos/268/100", title: "product-9" },
+{ src: "https://picsum.photos/400/400", title: "product-10" },
+]
+
 const ProductListPage = () => {
   return (
     <>
       <section className="w-full h-auto -mt-20">
         <img
           src="https://picsum.photos/1200/600"
-          className="object-cover w-full h-96"
+          className="object-cover w-full h-96 aspect-video bg-muted"
         ></img>
         <div className="mx-auto text-left text-sm mt-10 max-w-[90%] md:w-full md:text-center">
           <ProductDescText text="Crafted sweetness, elevated moments. "></ProductDescText>
@@ -28,30 +48,33 @@ const ProductListPage = () => {
       <section className="mt-20">
         <div className="mx-auto text-sm w-2/3">
           <h2>商品列表</h2>
-          {CardImage()}
+          <div className="mt-4 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {cards.map((item) => (
+              CardImage(item)   
+            ))}
+          </div>
         </div>
       </section>
     </>
   );
 };
 
-export function CardImage() {
+export function CardImage(item: CardItem) {
   return (
-    <Card className="relative mx-auto w-full max-w-sm pt-0">
+    <Card className="relative mx-auto w-full max-w-sm pt-0 flex flex-colks">
       <div className="absolute z-30 inset-0 aspect-video bg-black/35" />
       <img
-        src="/personal-website/home"
+        src={item.src}
         alt="Event cover"
-        className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+        className="h-full relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
       />
       <CardHeader>
         <CardAction>
-          <Badge variant="secondary">Featured</Badge>
+          {item.tag ? <Badge variant="secondary">{item.tag}</Badge>: <></> }
         </CardAction>
-        <CardTitle>Macha Mille Crepe Cake</CardTitle>
+        <CardTitle>{item.title}</CardTitle>
         <CardDescription>
-          A practical talk on component APIs, accessibility, and shipping
-          faster.
+          {item.desc}
         </CardDescription>
       </CardHeader>
     </Card>
